909ca1b11046028363c332067e987c03a3f15c27
Adnan Rahman <onlyshishir@gmail.com>
2024-07-14 22:08:10 +0000

    update:wq

---
+++ rentals/rentals/doctype/vehicle/vehicle.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2024, Adnan Rahman and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Vehicle(Document):
+	def before_save(self):
+		self.set_title()
+	
+	def set_title(self):
+		self.title = f"{self.make} {self.model}, {self.year}"
+
